b4ad3c9fdc44e41ae28f4dcb5f7fdc4222baa17a
najongjine <najongjin3@hotmail.com>
2020-02-28 16:38:20 +0900

    2020-2-28

---
+++ 2020-2-28 bbsfinal.sql
@@ -0,0 +1,11 @@
+create table tbl_comment(
+c_id	NUMBER		Primary Key,
+c_b_id	NUMBER	not null	,
+c_p_id	NUMBER	,
+c_date_time	VARCHAR2(30)	not null	,
+c_writer	nVARCHAR2(30)	not null	,
+c_subject	nVARCHAR2(125)	not null	
+);
+drop table tbl_comment;
+create sequence seq_comment
+start with 1 INCREMENT by 1;
